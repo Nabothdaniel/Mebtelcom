1993b44ffbcb6f3b0d9b228baaec84062d4be265
Nabothdaniel <nabothdaniel44@gmail.com>
2025-01-31 17:20:22 +0100

    fixed logout route error

--- backend/src/routes/Authroute.js
+++ backend/src/routes/Authroute.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post('/signup',signup);
 router.post('/login',authenticate, login);
-router.post('logout',authenticate,logout);
+router.post('/logout',authenticate,logout);
 
 export default router;
